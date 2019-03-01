cde1cf4b09365ec96ea2ae0a3c1c79ec86b033f3
survivorsfyh <survivorsfyh@163.com>
2019-03-01 18:57:41 +0800

    Commit new project

---
+++ YHEpunDemo/YHEpunDemoTests/YHEpunDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  YHEpunDemoTests.m
+//  YHEpunDemoTests
+//
+//  Created by survivors on 2019/3/1.
+//  Copyright © 2019年 survivorsfyh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YHEpunDemoTests : XCTestCase
+
+@end
+
+@implementation YHEpunDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
